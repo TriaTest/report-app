ca6dbafea87866d24b7db742c0dd431610c3e934
unknown <chantreahomdev@gmail.com>
2025-05-21 18:42:04 +0700

    Initial commit

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1 className="text-center text-2xl">Well come login page</h1>
+    )
+}
---
+++ src/app/(landing)/about/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1 className="text-center text-2xl">Well come about page</h1>
+    )
+}
